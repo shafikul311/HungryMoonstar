6f31cfa526267fbc4e1c836d9f9a7e74d1724012
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-02-08 17:17:45 +0600

    add some details

--- app.js
+++ app.js
@@ -1,33 +1,66 @@
+
 const mainDivFood = document.getElementById("mainDivFood");
 const warning = document.getElementById("warning");
 const searchBtn = document.getElementById("searchBtn");
 
 searchBtn.addEventListener("click", function () {
   const searchInput = document.getElementById("searchInput").value;
-  mainDivFood.innerHTML = ' ';
-  if(searchInput === ''){
-      warning.style.display = 'block';
-
-  }
-  else{
+  mainDivFood.innerHTML = " ";
+  if (searchInput === "") {
+    warning.style.display = "block";
+  } else {
     getFoods(searchInput);
-    warning.style.display = 'none'; 
-
+    warning.style.display = "none";
   }
   console.log(searchInput);
 });
 
-// searchBtn.addEventListener('click' , foodInp =>{
+// food Details info
+const displayDetails = (name) => {
+  
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${name}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => renderFoodInfo(data.meals[0]));
+};
 
-//     const foodName = document.getElementById('searchInput').value;
-//     // mainDivFood.innerHTML= '';
-//     // if(searchBtn ==== ''){
-//     //     warning.style.display = 'block'
-//     // }
+// Render foodInfo
+const renderFoodInfo = (food) => {
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (food[`strIngredient${i}`]) {
+      ingredients.push(
+        `${food[`strIngredient${i}`]} - ${food[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+    console.log(ingredients);
+  }
 
-//     console.log(foodName);
-// })
 
+  const foodDetails = document.getElementById("foodDetails");
+
+foodDetails.innerHTML = `
+<img  src="${food.strMealThumb}" alt="">
+<h4>${food.strMeal}</h4>
+
+<h5  Ingredients</h5>
+<ul >
+${ingredients
+  .map((ingredient) => `<li>${ingredient}</li>`)
+  .join("")}
+</ul>
+`;
+
+
+};
+
+
+
+
+
+// food call by id
 function getFoods(mealId) {
   const mainAPI = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealId}`;
 
@@ -58,30 +91,4 @@ function getFoods(mealId) {
   };
 }
 
-// // https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
-// fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`)
-// .then(res => res.json())
-// .then(data =>{
-//     displayFoods(data.meals);
-// })
 
-// const displayFoods = food =>{
-//     const foodDiv = document.getElementById('mainDivFood');
-//     if( food != null) {
-//         food.map(food =>{
-//             const mainDivFood = document.createElement('div');
-//             mainDivFood.className = 'mainFood'
-//             const foodInfo = `
-//             <div onclick="displayDetails('${food.idMeal}')">
-//             <img class="foodImg" src="${food.strMealThumb}">
-//             <h3>${food.strMeal}</h3>
-//             </div>
-//             `
-//             mainDivFood.innerHTML = foodInfo;
-//             foodDiv.appendChild(mainDivFood);
-//         })
-//     } else{
-//         warning.style.display = 'block';
-//     }
-
-// }
